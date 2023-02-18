e870fc1f948991e104d580e7a2eff617ce01ec66
NEKETA <molnei1996@gmail.com>
2023-02-19 01:31:41 +0300

    added zad3

---
+++ zad3/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+System.Console.WriteLine("Введите число");
+
+int znach = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i=1; i<=znach; i++)
+{   
+    System.Console.Write(Math.Pow(i,3)+" ");
+}
+
+
